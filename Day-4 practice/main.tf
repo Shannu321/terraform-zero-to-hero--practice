7853b76cc9df4c8f82f298b6155ef0e6ed95ad38
Shannu321 <shanmukhpisini.96@gmail.com>
2024-10-26 21:30:18 +0530

    Update main.tf

--- Day-4 practice/main.tf
+++ Day-4 practice/main.tf
@@ -5,5 +5,8 @@ provider "aws" {
 resource "aws_instance" "shanmukh" {
   instance_type = "t2.micro"
   ami = "ami-04a37924ffe27da53" # change this
+}
 
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "shanmukh-s3-demo-xyz"
 }
