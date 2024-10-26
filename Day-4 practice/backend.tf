ac9c7b01a9ac3038240f910c4738148337767279
Shannu321 <shanmukhpisini.96@gmail.com>
2024-10-26 21:14:13 +0530

    Update backend.tf

--- Day-4 practice/backend.tf
+++ Day-4 practice/backend.tf
@@ -1,8 +1,12 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "mybucket"
     key    = "Shanmukh/terraform.tfstate"
     region = "ap-south-1"
   }
+
+resource "aws_s3_bucket" "s3_bucket" {
+   bucket = " shanmukh-s3-demo-xyz"
+   }
 }
 
