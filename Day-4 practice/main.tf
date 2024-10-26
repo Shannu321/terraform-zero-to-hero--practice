9dbaf5e9e217c0d820febb95370ffa5f2aa68ba9
Shannu321 <shanmukhpisini.96@gmail.com>
2024-10-26 20:22:20 +0530

    Update main.tf

--- Day-4 practice/main.tf
+++ Day-4 practice/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "shanmukh" {
   instance_type = "t2.micro"
   ami = "ami-04a37924ffe27da53" # change this
 
